833f2622ddd2289f3b5cdcc12a847fd6c94290c4
Jeremy Tuloup <jeremy.tuloup@gmail.com>
2020-03-07 13:29:19 +0100

    Scale Unstable Kernel

--- sketches/circles/sketch.js
+++ sketches/circles/sketch.js
@@ -1,65 +1,73 @@
 const R = 500;
 const N = 100;
 const MAX = 200;
+const SCALE = 16 / 9 / 1000;
+
 let a = MAX;
 let x = 100;
 let off = 0.5;
-let CX, CY;
+
+let ratio;
 
 function setup() {
-    createCanvas(windowWidth, windowHeight);
-    CX = innerWidth / 2;
-    CY = innerHeight / 2;
+  createCanvas(windowWidth, windowHeight);
+  windowResized();
 }
 
-function easeInOutCubic (t) {
-    return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1;
+function windowResized() {
+  ratio = SCALE * min(innerWidth, innerHeight);
+  resizeCanvas(innerWidth, innerHeight);
+}
+
+function easeInOutCubic(t) {
+  return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
 }
 
 function keyPressed() {
-    a = MAX;
-    x = int(random(1000));
-    randomSeed();
-    off = random(1)
+  a = MAX;
+  x = int(random(1000));
+  randomSeed();
+  off = random(1);
 }
 
 function makeCircle(s) {
-    strokeWeight(s);
-    stroke(255);
-    noFill();
-    let r = R * (0.25 + off * 2);
-    ellipse(0, 0, r, r);
+  strokeWeight(s);
+  stroke(255);
+  noFill();
+  let r = R * (0.25 + off * 2);
+  ellipse(0, 0, r, r);
 }
 
-function mousePressed () {
-    noLoop();
+function mousePressed() {
+  noLoop();
 }
 
 function mouseReleased() {
-    loop();
+  loop();
 }
 
 function draw() {
-    a *= 0.95;
-    let b = easeInOutCubic(a / MAX) * MAX;
-    let c = color(b, b, b, 50);
-    background(c);
+  a *= 0.95;
+  const b = easeInOutCubic(a / MAX) * MAX;
+  const c = color(b, b, b, 50);
+  background(c);
 
-    translate(CX, CY);
-    randomSeed(42);
+  translate(innerWidth / 2, innerHeight / 2);
+  scale(ratio * 0.5);
+  randomSeed(42);
 
-    let tick = frameCount * off;
-    for (let i = 0; i < N; i++) {
-        push();
-        let dir = i % 2  === 0 ? -1 : 1;
-        rotate(dir * random(N / 4) * tick * 0.004);
+  const tick = frameCount * off;
+  for (let i = 0; i < N; i++) {
+    push();
+    const dir = i % 2 === 0 ? -1 : 1;
+    rotate(dir * random(N / 4) * tick * 0.004);
 
-        let amp = sin(tick * 0.001 * random(10)) * 6;
-        let x = random(-100, 100) + random(N / 40) * exp(amp);
-        let y = random(-100, 100);
-        translate(x, y);
-        scale(i / 500);
-        makeCircle(random(20));
-        pop();
-    }
+    const amp = sin(tick * 0.001 * random(10)) * 6;
+    const x = random(-100, 100) + random(N / 40) * exp(amp);
+    const y = random(-100, 100);
+    translate(x, y);
+    scale(i / 500);
+    makeCircle(random(20));
+    pop();
+  }
 }
